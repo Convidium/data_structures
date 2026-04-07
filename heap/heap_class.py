0b3752a44e0d89dcb85570296bd72f9e1f960a77
Convidium <denyssianchuk0@gmail.com>
2026-04-07 12:53:36 +0200

    Implementation of Mix/Max Heap with abstract class BaseHeap

--- heap/heap_class.py
+++ heap/heap_class.py
@@ -1,24 +1,21 @@
-class MaxHeap:
+from abc import ABC, abstractmethod
+
+class BaseHeap(ABC):
     def __init__(self, value):
         self.heap = []
         if isinstance(value, list) and len(value) > 0: 
             self.build_heap(value)
     
     def parent(self, index):
-        if index == 0: return self.heap[index]
+        if index == 0: return 0
         if len(self.heap) > 0:
             return (index-1)//2
         
     def insert(self, value):
         self.heap.append(value)
         self.heapify_up(len(self.heap)-1)
-            
-    def heapify_up(self, index):
-        while index != 0 and self.heap[self.parent(index)] < self.heap[index]:
-            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]        
-            index = self.parent(index)
-            
-    def extract_max(self):
+        
+    def extract(self):
         if not self.heap:
             return None
         
@@ -27,23 +24,28 @@ class MaxHeap:
         self.heap.pop()
         self.heapify_down(0)
         return maximum
+            
+    def heapify_up(self, index):
+        while index != 0 and self._compare(self.heap[self.parent(index)], self.heap[index]):
+            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]        
+            index = self.parent(index)
     
     def heapify_down(self, index, heap_size=None):
         if heap_size is None:
             heap_size = len(self.heap)
-        largest = index
+        extreme_idx = index
         
         while True:
             left = 2*index+1
             right = 2*index+2
             
-            if left < heap_size and self.heap[left] > self.heap[largest]:
-                largest = left
-            if right < heap_size and self.heap[right] > self.heap[largest]:
-                largest = right
-            if largest != index:
-                self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
-                index = largest
+            if left < heap_size and self._compare(self.heap[extreme_idx], self.heap[left]):
+                extreme_idx = left
+            if right < heap_size and self._compare(self.heap[extreme_idx], self.heap[right]):
+                extreme_idx = right
+            if extreme_idx != index:
+                self.heap[index], self.heap[extreme_idx] = self.heap[extreme_idx], self.heap[index]
+                index = extreme_idx
             else:
                 break
     
@@ -64,3 +66,15 @@ class MaxHeap:
         
     def __repr__(self):
         return f"Heap: {self.heap}"
+    
+    @abstractmethod
+    def _compare(self, parent_val, child_val):
+        pass
+    
+class MaxHeap(BaseHeap):
+    def _compare(self, parent_val, child_val):
+        return parent_val < child_val
+
+class MinHeap(BaseHeap):
+    def _compare(self, parent_val, child_val):
+        return parent_val > child_val
